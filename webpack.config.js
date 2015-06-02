c4edd083d2cf84c3b021d0b1fa15061c3c89d510
Rafael Chiti <rafaelchiti@gmail.com>
2015-06-02 15:17:48 -0700

    Simplifying

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ var webpackConfig = {
     path: './build',
     filename: 'bundle.js',
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-source-map',
   module: {
     loaders: [
       // IMPORTANT: we don't want to process EVERY single JS file with babel
       // loader. We only want to process the files inside our app structure
       // otherwise this could get very slow or even fail.
-      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader?optional=runtime']},
+      {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
     ]
   }
 };
