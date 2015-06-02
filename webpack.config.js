fdc072b8b7c7ba67ba1c5b160013e44b13740782
Rafael Chiti <rafaelchiti@gmail.com>
2015-06-02 15:21:28 -0700

    Remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,6 @@ var webpackConfig = {
   devtool: 'eval-source-map',
   module: {
     loaders: [
-      // IMPORTANT: we don't want to process EVERY single JS file with babel
-      // loader. We only want to process the files inside our app structure
-      // otherwise this could get very slow or even fail.
       {test: /\.jsx?$/, exclude: /node_modules/, loaders: ['babel-loader']},
     ]
   }
